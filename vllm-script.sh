7f6979668bc22a9b035c080a5d2323259b27298b
Farzad Shami <farzad.shami@aalto.fi>
2026-05-01 10:48:05 +0300

    vllm script

---
+++ vllm-script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --account=project_2018216
+#SBATCH --partition=gputest
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=128
+#SBATCH --time=15
+#SBATCH --gres=gpu:a100:4
+
+source /projappl/project_2018216/GeoAI/bin/activate
+
+# Generate a random API key for the vLLM server and output it
+export VLLM_API_KEY=$(mktemp -u XXXXXXXXXXXX)
+echo "### THE API KEY IS ###"
+echo $VLLM_API_KEY
+echo "######################"
+
+# Where to store the huge models. Point this to your project's scratch directory.
+# For example Deepseek-R1-Distill-Llama-70B requires 132GB
+export HF_HOME=/scratch/$SLURM_JOB_ACCOUNT/hf-cache
+
+MODEL="Qwen/Qwen3.5-4B"
+
+srun vllm serve $MODEL \
+       --port 8000 \
+       --tensor-parallel-size 4 \
+       --max-model-len 32768 \
+       --gpu_memory_utilization 0.98 \
+       --enforce-eager
